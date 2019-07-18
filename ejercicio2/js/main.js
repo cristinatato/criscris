2ff78b87c3eb5c9882f02519b4983b18864ae4c2
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-07-18 09:29:11 +0200

    add ej2

---
+++ ejercicio2/js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let cris1 = 'Calle río duero, 59';
+
+document.querySelector('.address').innerHTML = cris1;
+
+document.querySelector('.address').innerHTML = 'paseo de valdelasfuentes';
