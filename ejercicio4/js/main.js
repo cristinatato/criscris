0f0e08293aeaba9b2a6fd5e06d746c415354a670
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-07-18 10:07:57 +0200

    add ej4

---
+++ ejercicio4/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+const win = document.querySelector('.winner');
+
+/* let adalaber = document.querySelector('.adalaber');
+
+win.innerHTML = win.innerHTML + ' ' + adalaber.innerHTML;
+ */
+
+let adalaber = document.querySelector('.adalaber:nth-child(2)');
+
+
+win.innerHTML = win.innerHTML + ' ' + adalaber.innerHTML;
