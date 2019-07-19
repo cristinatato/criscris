6874f772b16865b17a3b95f728c173ca580acc6c
cristinatato <c.tatodelatorre@gmail.com>
2019-07-19 09:44:30 +0200

    add ej4

---
+++ 2.2/ej4/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const total = 128;
+
+const ana = 2;
+
+const personas = 9;
+
+const aPagar = (total - ana) / personas;
+
+
+console.log (`Cada persona tiene que pagar ${aPagar} € y Ana tiene que pagar ${aPagar + ana}`);
+
+
