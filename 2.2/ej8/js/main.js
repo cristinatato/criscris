d503f8edfb2df6039ad3ff140fc61b305ec00844
cristinatato <c.tatodelatorre@gmail.com>
2019-07-19 23:05:59 +0200

    add ej8

---
+++ 2.2/ej8/js/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const firstDogImage = 'https://images.dog.ceo/breeds/schipperke/n02104365_8156.jpg';
+const firstDogName = 'Dina';
+
+const secondDogImage = 'https://images.dog.ceo/breeds/collie-border/n02106166_355.jpg';
+const secondDogName = 'Luna';
+
+const thirdDogImage = 'https://images.dog.ceo/breeds/affenpinscher/n02110627_7065.jpg';
+const thirdDogName = 'Lana';
+
+const list = document.querySelector ('.list');
+
+list.innerHTML = `
+<p>Mis perros se llaman ${firstDogName}, ${secondDogName} y ${thirdDogName}</p>
+<p>Y estas son sus fotos:</p>
+<ul>
+    <li>
+        <img src"${firstDogImage}" alt"${firstDogName}">
+    </li>
+    <li>
+        <img src"${secondDogImage}" alt"${secondDogName}">
+    </li>
+    <li>
+        <img src"${thirdDogImage}" alt"${thirdDogName}">
+    </li>  
+</ul>
+`;
