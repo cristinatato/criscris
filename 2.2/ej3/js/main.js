885290f57e8cfc92ab7849e53f1d55df5910b82d
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-07-19 09:31:21 +0200

    ej3 module 2.2

---
+++ 2.2/ej3/js/main.js
@@ -0,0 +1,12 @@
+const kiwis = 5;
+const peras = 2;
+const uvas = 4;
+
+const tKiwis = 2*kiwis;
+const tPeras = 3*peras;
+const tUvas = 0.5*uvas;
+
+console.log('kiwis', tKiwis, '€');
+console.log('peras', tPeras, '€');
+console.log('uvas', tUvas, '€');
+console.log('total', tKiwis + tPeras + tUvas, '€');
