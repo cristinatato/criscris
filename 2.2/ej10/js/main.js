a0de88d0dd5b07f49e4a2b8c0e8fecdd7f1b9ed1
cristinatato <c.tatodelatorre@gmail.com>
2019-07-20 14:54:57 +0200

    add ej10

---
+++ 2.2/ej10/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const age = 24;
+const año = 365;
+const dia = 24;
+const hoursLife = (age * año * dia);
+const life = document.querySelector ('.life');
+
+life.innerHTML = `
+<p>Tengo ${hoursLife} horas de vida.</p>
+`;
