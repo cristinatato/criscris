46632d117679482a0bbe3219e8b3c0a757fdbbcd
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-07-21 18:22:43 +0200

    ej 8, 9 and 10

---
+++ 2.2/ej10/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const age = 24;
+const año = 365;
+const dia = 24;
+
+document.querySelector('.title').innerHTML = `Tengo ${age * año * dia} horas de vida`;
+
+/* console.log(`Tengo ${age * año * dia} horas de vida`); */
---
+++ 2.2/ej9/js/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const name = 'Cristina Tato';
+
+document.querySelector('.title').innerHTML = `El nombre de mi compañera es: ${name} y tiene ${name.length} caracteres.`;
