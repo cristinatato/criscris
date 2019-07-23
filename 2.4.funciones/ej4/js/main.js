822fbc4ca9042a1d83e8bccc2d1865ac5f0223bb
cristinatato <c.tatodelatorre@gmail.com>
2019-07-23 09:47:34 +0200

    add exercise 4

---
+++ 2.4.funciones/ej4/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function parImpar(a) {
+    if ((a%2)===0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log (parImpar(5));
+console.log (parImpar(16));
