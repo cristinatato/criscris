af9cad2f6b7c67df3796e9c208b0d3638703fa65
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-07-23 09:42:15 +0200

    ej 3 module 2.4

---
+++ 2.4.funciones/ej3/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function price (a) {
+    const sinIVA = a;
+    const iVA = a * (21/100);
+    const total = sinIVA + iVA;
+    return (`El precio sin IVA: ${sinIVA}, IVA: ${iVA} y Total: ${total}`);
+}
+
+const result1 = price (10);
+console.log(result1);
+
+const result2 = price (20);
+console.log(result2);
+
+const result3 = price (100);
+console.log(result3);
